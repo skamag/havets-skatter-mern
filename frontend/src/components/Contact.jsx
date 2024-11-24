40720021a5f1c331c76c26474e562e88d67a7be3
unknown <magneskaro@gmail.com>
2024-11-24 11:44:50 +0100

    mobile iframe postal code change

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -79,7 +79,7 @@ export default function Contact() {
         <div className="iframeContainer-narrow">
           <iframe
             className="iframe"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.626049537636!2d10.72204596341659!3d59.90826566438507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e81bceae4f9%3A0xe68ffef57f364675!2sAker%20brygge%2C%20Oslo!5e1!3m2!1sno!2sno!4v1731924964014!5m2!1sno!2sno"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30767.87699612734!2d10.686101816428504!3d59.88891751470016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416bb52938958f%3A0x451b9d4daaf1fb40!2s0150%20Oslo!5e0!3m2!1sno!2sno!4v1732099342380!5m2!1sno!2sno"
             // width="600"
             // height="450"
             style={{ border: 0 }}
